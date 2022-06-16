905f024188cdf07a9a2577eb911c1e9f96af0374
Keshav R Karanth <48642293+keshavrkaranth@users.noreply.github.com>
2022-06-16 09:11:45 +0530

    renamed copyright name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () =>{
       <Container>
         <Row>
           <Col className="text-center py-3">
-            Copyright &copy; Keshav R Karanth
+            Copyright &copy; Proshop
           </Col>
         </Row>
       </Container>
